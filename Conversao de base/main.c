070a7dc54493dd5d175ec1946f0d089c8344ec08
lucas-limabr <lucaslimamg28@gmail.com>
2023-11-15 16:49:47 -0300

    ajustando controles de execução

--- Conversao de base/main.c
+++ Conversao de base/main.c
@@ -24,7 +24,7 @@ int main()
 int conversaoInteiro(int parteinteira, float numero)
 {
     //fazendo o módulo da parte inteira
-    int conversaoBin[23], i, quociente = abs(parteinteira);
+    int conversaoBin[22], i, quociente = abs(parteinteira);
 
     //CONVERSÃO BINÁRIA DA PARTE INTEIRA
     for(i = 0; quociente >= 2; i++)
@@ -34,54 +34,83 @@ int conversaoInteiro(int parteinteira, float numero)
     }
     conversaoBin[i] = quociente;
 
-    //criei o vetor que terá exatamente a msm quantidade de bits da conversão
-    int ordemCorreta[i];
     int j, k = i;
     //backup que será usado posteriormente
     int l = k;
 
-    //faço a cópia para este vetor, porém até a última posição significativa, desconsiderando lixos
-    //de memória.Porém eu faço o reverse do vetor, pois a conversão binária é de trás para frente
+    if(parteinteira == 1)
+    {
+        i++;
+    }
+    //criei o vetor que terá exatamente a msm quantidade de bits da conversão
+    int ordemCorreta[i];
+
+    //faço a cópia para este vetor, porém até a última posição significativa, desconsiderando as
+    //posições que não foram preechidas no vetor conversaoBin.Porém eu faço o reverse do vetor,
+    //pois a conversão binária é de trás para frente
     for(j = 0; j <= k; j++, i--)
     {
         ordemCorreta[j] = conversaoBin[i];
         printf("\n%d", ordemCorreta[j]);
     }
 
+
+
     //CONVERSÃO BINÁRIA DA PARTE DECIMAL
     float parteDecimal = fabs(numero) - (abs(parteinteira));
 
     printf("\nDecimal (base 10): %.2f", fabs(parteDecimal));
 
-    float result_parcial = parteDecimal;
+    float result_parcial = fabs(parteDecimal);
 
-    k++;
     int mantissa = 23 - k;
     int conversaoBinDecimal[mantissa];
 
-    for(i = 0; i < mantissa || result_parcial == 1; i++){
-        if(result_parcial > 1){
+    for(i = 0; i < mantissa; i++)
+    {
+        if(result_parcial == 1.00)
+        {
+            conversaoBinDecimal[i] = 1;
+            i++;
+            for(i; i < mantissa; i++)
+            {
+                conversaoBinDecimal[i] = 0;
+            }
+            break;
+        }
+
+        if(result_parcial > 1)
+        {
             result_parcial -= 1.00;
         }
         result_parcial = result_parcial * 2;
 
-        if(result_parcial < 1.00){
+        if(result_parcial < 1.00)
+        {
             conversaoBinDecimal[i] = 0;
         }
-        else{
+        else
+        {
             conversaoBinDecimal[i] = 1;
         }
+    }
 
-        printf("\n%d", conversaoBinDecimal[i]);
+    //APENAS PARA EXIBIÇÃO
+    for(i = 0; i < mantissa; i++)
+    {
+        printf("\nConversão da parte decimal: %d", conversaoBinDecimal[i]);
     }
 
     //BIAS
     //EXP. REPRESENTADO = EXP. REAL + BIAS(127)
     int exp_real = 0;
     j = 0;
-    for(i = 1; ordemCorreta[j] != 1; j++){
+    for(i = 0; ordemCorreta[j] != 1; j++)
+    {
         i++;
     }
+    //backup de i mais um, será usado posteriormente
+    int auxiliar = (i + 1);
     exp_real = k - i;
 
     int exp_representado = exp_real + 127;
@@ -89,17 +118,22 @@ int conversaoInteiro(int parteinteira, float numero)
     int bitSinal[8];
     quociente = exp_representado;
 
-    for(i = 0; quociente >= 2 && i < 8; i++)
+    for(i = 0; quociente >= 2 && i < 7; i++)
     {
         bitSinal[i] = quociente % 2;
         quociente = quociente / 2;
     }
     bitSinal[i] = quociente;
 
-    int ordemCorreta2[i];
-    k = i;
+    if(i == 6)
+    {
+        i++;
+        bitSinal[i] = 0;
+    }
 
-    for(j = 0; j <= k; j++, i--)
+    int ordemCorreta2[8];
+
+    for(j = 0; j < 8; j++, i--)
     {
         ordemCorreta2[j] = bitSinal[i];
         printf("\nExpoente: %d", ordemCorreta2[j]);
@@ -109,19 +143,22 @@ int conversaoInteiro(int parteinteira, float numero)
     //ordemCorreta E conversaoBinDecimal
     int mantissaCompleta[23];
 
-    l++;
     printf("\n l: %d", l);
-    for(i = 0; i < l; i++){
-        mantissaCompleta[i] = ordemCorreta[i];
+    j = 0;
+    for(i = auxiliar; i < l; i++, j++)
+    {
+        mantissaCompleta[j] = ordemCorreta[i];
     }
 
-    j = 0;
-    for(i = l; i < 23; i++, j++){
-        mantissaCompleta[i] = conversaoBinDecimal[j];
+    i = 0;
+    for(j; j < 23; j++, i++)
+    {
+        mantissaCompleta[j] = conversaoBinDecimal[j];
     }
 
     printf("\nMantissona\n");
-    for(i = 0; i < 23; i++){
+    for(i = 0; i < 23; i++)
+    {
         printf("%d", mantissaCompleta[i]);
     }
     printf("\n");
@@ -131,33 +168,38 @@ int conversaoInteiro(int parteinteira, float numero)
     int mantissa_e_sinal[32];
 
 
-    if(numero > 1.0){
+    if(numero >= 0)
+    {
         mantissa_e_sinal[0] = 0;
     }
-    else if(numero < 0.0){
+    else
+    {
         mantissa_e_sinal[0] = 1;
     }
 
     j = 0;
     i = 1;
 
-       while(j < 8){
-       mantissa_e_sinal[i] = ordemCorreta2[j];
-       j++;
-       i++;
-       }
+    while(j < 8)
+    {
+        mantissa_e_sinal[i] = ordemCorreta2[j];
+        j++;
+        i++;
+    }
 
-       j = 0;
+    j = 0;
 
-       while(i < 32){
-       mantissa_e_sinal[i] = mantissaCompleta[j];
-       i++;
-       j++;
-       }
+    while(i < 32)
+    {
+        mantissa_e_sinal[i] = mantissaCompleta[j];
+        i++;
+        j++;
+    }
 
-       for(i = 0; i < 32; i++){
-       printf("\nMantissa e expoente: %d", mantissa_e_sinal[i]);
-       }
+    for(i = 0; i < 32; i++)
+    {
+        printf("\nMantissa e expoente: %d", mantissa_e_sinal[i]);
+    }
 
     return ordemCorreta;
 }
